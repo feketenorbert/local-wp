e24def559f895c3cdb0bbfd8bf6259d3099da68c
Silviu-Gorea <silviu-gorea@DHM-9603.local>
2026-07-01 14:58:09 +0300

    Adaugare hello elementor si child theme

--- wp-content/mu-plugins/99-studio-loader.php
+++ wp-content/mu-plugins/99-studio-loader.php
@@ -17,7 +17,7 @@
 		// Set environment type to local if not already defined
 		if ( ! defined( 'WP_ENVIRONMENT_TYPE' ) ) define( 'WP_ENVIRONMENT_TYPE', 'local' );
 
-		$studio_mu_plugins_dir = '/var/folders/fp/c80_hct928g1454z3jj1r9000000gn/T/studio-mu-plugins-ZXKfJ5';
+		$studio_mu_plugins_dir = '/var/folders/r5/6tb7qpdn14j5qqdc9mjzll9h0000gn/T/studio-mu-plugins-Hm8Gka';
 
 		if ( is_dir( $studio_mu_plugins_dir ) ) {
 			$files = glob( $studio_mu_plugins_dir . '/*.php' );
