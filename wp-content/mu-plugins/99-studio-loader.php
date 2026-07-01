755f41ab89c2c658c2060d3fc017c14a7d4ddfb7
Vanesa-Broasca <softgenius@DHM-7431.local>
2026-07-01 14:35:29 +0300

    added child theme and elementor

--- wp-content/mu-plugins/99-studio-loader.php
+++ wp-content/mu-plugins/99-studio-loader.php
@@ -17,7 +17,7 @@
 		// Set environment type to local if not already defined
 		if ( ! defined( 'WP_ENVIRONMENT_TYPE' ) ) define( 'WP_ENVIRONMENT_TYPE', 'local' );
 
-		$studio_mu_plugins_dir = '/var/folders/fp/c80_hct928g1454z3jj1r9000000gn/T/studio-mu-plugins-ZXKfJ5';
+		$studio_mu_plugins_dir = '/var/folders/r3/phm49ycs7l1d6hwr3cpr0tvr0000gn/T/studio-mu-plugins-jfKj0O';
 
 		if ( is_dir( $studio_mu_plugins_dir ) ) {
 			$files = glob( $studio_mu_plugins_dir . '/*.php' );
